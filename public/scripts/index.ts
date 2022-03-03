29b206c11004cfa6b37176e599e6e8999ec10e07
Tibo Ulens <ulens.tibo@gmail.com>
2022-03-03 21:29:14 +0100

    finalise probability visualization

--- public/scripts/index.ts
+++ public/scripts/index.ts
@@ -2,41 +2,14 @@
  * Visualiser for viterbi probability data in 12ul
  */
 
+import { interpolate_hsl, interpolate_rgb, hex2rgb } from "./color";
+import { Config, Probabilities } from "./types";
+
 const VITERBI_CONFIG_URL = "http://10.1.0.200:8080/lappers/viterbi/configuration";
 const VITERBI_PROBABILITIES_URL = "http://10.1.0.200:8080/lappers/viterbi/probabilities";
 
-/**
- * The probabilities of any team being in a given sector
- */
-type Probabilities = {
-	/**
-	 * The name of the team
-	 */
-	[key: string]: {
-		/**
-		 * The name of the sector
-		 */
-		[key: string]: number
-	}
-};
-
-/**
- * The config of the viterbi lapper
- */
-type Config = {
-    TRACK_LENGTH: number;
-    SECTOR_STARTS: number[];
-    AVERAGE_RUNNER_SPEED: number;
-    DETECTIONS_PER_SECOND: number;
-    STATION_RANGE_SIGMA: number;
-    RESTART_PROBABILITY: number;
-};
-
-const _PROBABILITY_STUB: Probabilities = {"1":{"0":0.996,"1":0.001,"2":0.001,"3":0.001,"4":0.001},"2":{"0":0.996,"1":0.001,"2":0.001,"3":0.001,"4":0.001},"3":{"0":0.996,"1":0.001,"2":0.001,"3":0.001,"4":0.001},"4":{"0":0.996,"1":0.001,"2":0.001,"3":0.001,"4":0.001},"5":{"0":0.996,"1":0.001,"2":0.001,"3":0.001,"4":0.001}};
+const _PROBABILITY_STUB: Probabilities = {"1":{"0":0.5,"1":0.3,"2":0.1,"3":0.05,"4":0.05},"2":{"0":0.05,"1":0.1,"2":0.15,"3":0.4,"4":0.3},"3":{"0":0.2,"1":0.2,"2":0.2,"3":0.2,"4":0.2},"4":{"0":0.005,"1":0.005,"2":0.09,"3":0.7,"4":0.2},"5":{"0":0.1,"1":0.2,"2":0.3,"3":0.2,"4":0.2}};
 const _CONFIG_STUB: Config = {"TRACK_LENGTH":500,"SECTOR_STARTS":[0,100,150,250,350],"AVERAGE_RUNNER_SPEED":100.0,"DETECTIONS_PER_SECOND":1.0,"STATION_RANGE_SIGMA":50.0,"RESTART_PROBABILITY":0.001};
-
-const CANVAS = <HTMLCanvasElement>document.getElementById("canvas");
-const CONTEXT = CANVAS.getContext("2d")!;
 
 /**
  * Get the viterbi config from the server or use stub data on failure
@@ -149,14 +122,43 @@ function draw_team_boundaries (
 	return team_interval_map;
 }
 
-async function main(cvs: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+function draw_probabilities(
+	ctx: CanvasRenderingContext2D,
+	background: [number, number, number],
+	colors: [number, number, number][],
+	sector_interval_map: {[key: string]: [number, number]},
+	team_interval_map: {[key: string]: [number, number]},
+	probabilities: Probabilities,
+) {
+	let color_idx = 0;
+
+	for (const [team, probs] of Object.entries(probabilities)) {
+		for (const [sector, prob] of Object.entries(probs)) {
+			ctx.fillStyle = interpolate_rgb(background, colors[color_idx], prob);
+			ctx.fillRect(
+				sector_interval_map[sector][0] + 5,
+				team_interval_map[team][1] + 5,
+				sector_interval_map[sector][1] - sector_interval_map[sector][0] - 10,
+				team_interval_map[team][0] - team_interval_map[team][1] - 10,
+			);
+		}
+		color_idx++;
+	}
+}
+
+async function main() {
+	const cvs = <HTMLCanvasElement>document.getElementById("canvas");
+	const ctx = cvs.getContext("2d")!;
+
+	const background = "#0E0E0E";
+
 	// Start awaiting viterbi data
 	const [prb_promise, cfg_promise] = [get_probabilities(), get_config()];
 
 	// Initial canvas setup
-	cvs.width = window.innerWidth*2/3;
+	cvs.width = window.innerWidth;
 	cvs.height = window.innerHeight;
-	ctx.fillStyle = "#0E0E0E";
+	ctx.fillStyle = background;
 	ctx.fillRect(0, 0, cvs.width, cvs.height);
 	ctx.strokeStyle = "#FFFFFF";
 	ctx.fillStyle = "#FFFFFF";
@@ -169,7 +171,11 @@ async function main(cvs: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
 
 	const [prb, cfg] = await Promise.all([prb_promise, cfg_promise]);
 
+
+	// Used to calculate x offset of origin
 	const max_text_len = Math.max(...Object.keys(prb).map((team) => ctx.measureText(team).width))
+
+	// Used to calculate y offset of origin
 	const sector_max_text_height = Math.max(...Object.keys(Object.values(prb)[0]).map((sector) => {
 		let metrics = ctx.measureText(sector);
 		return metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
@@ -179,8 +185,12 @@ async function main(cvs: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
 		return metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
 	}));
 	const max_text_height = Math.max(sector_max_text_height, team_max_text_height);
+
+	/** Origin coordinates */
 	const origin: [number, number] = [x_spacer*2 + max_text_len, cvs.height - y_spacer*2 - max_text_height]
+	/** Start and end coordinates of x axis */
 	const x_interval: [number, number] = [origin[0], cvs.width - x_spacer];
+	/** Start and end coordinates of y axis */
 	const y_interval: [number, number] = [y_spacer, origin[1]];
 
 	// Sector axis
@@ -195,16 +205,27 @@ async function main(cvs: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
 	ctx.lineTo(x_interval[0], y_interval[0]);
 	ctx.stroke();
 
+	/** Map between sector name and normalised sector start position */
 	const normalised_sector_map: {[key: string]: number} = {};
 	for (const [idx, start] of Object.entries(cfg.SECTOR_STARTS)) {
 		normalised_sector_map[idx] = start / cfg.TRACK_LENGTH;
 	}
 
+	// Draw sectors on graph and get intervals
 	const sector_interval_map = draw_sector_boundaries(ctx, origin, x_interval, normalised_sector_map, y_spacer);
+	// Draw teams on graph and get intervals
 	const team_interval_map = draw_team_boundaries(ctx, origin, y_interval, Object.keys(prb), x_spacer);
 
-	console.log(cfg);
-	console.log(prb);
+	// Get array of colors to use for drawing probabilites
+	const team_colors: [number, number, number][] = [];
+	const color_interval = 1 / (Object.keys(prb).length-1);
+	let color_factor = 0;
+	for (let i=0; i<Object.keys(prb).length; i++) {
+		team_colors.push(interpolate_hsl([255, 0, 0], [0, 255, 255], color_factor));
+		color_factor += color_interval;
+	}
+
+	draw_probabilities(ctx, hex2rgb(background), team_colors, sector_interval_map, team_interval_map, prb);
 }
 
-window.onload = async () => await main(CANVAS, CONTEXT);
+window.onload = main;
